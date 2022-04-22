3363d9ae27e007489d4daf1aae18b64d138d0a75
Evilcarry <benjhithaxx@gmail.com>
2022-04-22 12:56:40 +1200

    Update LoginScreen.kt
    Added some comments for clarity

--- app/src/main/java/com/example/athopnfc/LoginScreen.kt
+++ app/src/main/java/com/example/athopnfc/LoginScreen.kt
@@ -5,13 +5,19 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
+//Here we can see the AppCompatActivity() extend
 class LoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.log_in_screen)
+
+        //Here I am declaring a button and giving it the XML button via ID, in your case do findViewByID(R.id.idofthebutton)
         val sUButton: Button = findViewById(R.id.signUpButton)
+        //Now that I have the button object I can access the action listener
         sUButton.setOnClickListener {
+            //Making an intent object i think, well you just give it this class and the next one.
             val intent = Intent(this, CreateAccount::class.java)
+            //dont forget this part which makes it run.
             startActivity(intent)
             }
     }
@@ -20,6 +26,7 @@ class LoginScreen : AppCompatActivity() {
 class CreateAccount : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //In here make sure to set the content to the new XML screen.
         setContentView(R.layout.create_account_screen)
     }
 }
