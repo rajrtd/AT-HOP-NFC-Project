af21d2d7a6b236c5a533d7579a2022ce396400dc
Raj Rathod <rajrasikrathod@gmail.com>
2022-04-22 15:30:36 +1200

    Working on Add Card Screen

--- app/src/main/java/com/example/athopnfc/MainActivity.kt
+++ app/src/main/java/com/example/athopnfc/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.athopnfc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.athopnfc.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.add_card)
     }
 }
---
+++ app/src/main/java/com/example/athopnfc/kotlinfile.kt
@@ -0,0 +1,5 @@
+package com.example.athopnfc
+
+fun main(){
+    println("Hello World")
+}
