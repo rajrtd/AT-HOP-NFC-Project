e7df297b9cc10d2731db397ce003c775c4f041db
Evilcarry <benjhithaxx@gmail.com>
2022-05-02 15:51:01 +1200

    Added the logout button
    sort of

--- app/src/main/java/com/example/athopnfc/MainActivity.kt
+++ app/src/main/java/com/example/athopnfc/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.athopnfc
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
