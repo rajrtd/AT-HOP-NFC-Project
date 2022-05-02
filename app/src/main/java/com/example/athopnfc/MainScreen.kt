89205a2af6bf024324cb5f805516aa48e3590d62
Evilcarry <benjhithaxx@gmail.com>
2022-05-02 15:21:38 +1200

    fixing main

--- app/src/main/java/com/example/athopnfc/MainScreen.kt
+++ app/src/main/java/com/example/athopnfc/MainScreen.kt
@@ -1,31 +1,43 @@
 package com.example.athopnfc
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import androidx.viewpager2.widget.ViewPager2
+import com.example.athopnfc.fragments.TabPageAdapter
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.navigation_layout.*
 
 class MainScreen : AppCompatActivity(){
 
-    //LOGOUT BUTTON THINGS
-    private lateinit var btnLogOut : Button
-    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_screen)
+        setContentView(R.layout.navigation_layout)
+        setUpTabBar()
+    }
 
-        auth = Firebase.auth
+    private fun setUpTabBar()
+    {
+        val adapter = TabPageAdapter(this, tabLayout.tabCount)
+        viewPager.adapter = adapter
 
-        btnLogOut = findViewById(R.id.btnLogOut)
-        btnLogOut.setOnClickListener {
-            auth.signOut()
-            val logoutIntent = Intent(this, LoginScreen::class.java)
-            logoutIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(logoutIntent)
-        }
+        viewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback()
+        {
+            override fun onPageSelected(position: Int) {
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener
+        {
+            override fun onTabSelected(tab: TabLayout.Tab)
+            {
+                viewPager.currentItem = tab.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
     }
 }
