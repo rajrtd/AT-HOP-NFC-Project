0e9eaf3177fc95b7ef8d8b1cd6ca6e61e9c77b8b
Raj Rathod <rajrasikrathod@gmail.com>
2022-05-04 11:03:22 +1200

    hop card add screen has been added

--- app/src/main/java/com/example/athopnfc/SelectCardScreen.kt
+++ app/src/main/java/com/example/athopnfc/SelectCardScreen.kt
@@ -1,15 +1,26 @@
 package com.example.athopnfc
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SelectCardScreen :AppCompatActivity() {
+
+    private lateinit var addHopCardBtn: Button;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.add_cards)
+
+        addHopCardBtn = findViewById(R.id.hopCard);
+
+        addHopCardBtn.setOnClickListener {
+            val intent = Intent(this, AddHopCard::class.java)
+            startActivity(intent)
+        }
     }
 
 }
