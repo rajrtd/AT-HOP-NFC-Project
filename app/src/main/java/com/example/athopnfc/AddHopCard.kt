61af55f25f0fd12462472b4e7ac12d80632b92e9
Raj Rathod <rajrasikrathod@gmail.com>
2022-05-04 11:03:22 +1200

    hop card add screen has been added

---
+++ app/src/main/java/com/example/athopnfc/AddHopCard.kt
@@ -0,0 +1,12 @@
+package com.example.athopnfc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddHopCard : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_athop_card)
+    }
+}
