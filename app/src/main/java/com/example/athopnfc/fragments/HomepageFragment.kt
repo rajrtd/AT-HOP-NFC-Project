e06e818942aefc3d8f3adbfeb485777ebe518b3c
Evilcarry <benjhithaxx@gmail.com>
2022-05-03 20:52:14 +1200

    added logout button
    Added the logout button in homescreen for debugging purposes for now, will be moved to another place later.

--- app/src/main/java/com/example/athopnfc/fragments/HomepageFragment.kt
+++ app/src/main/java/com/example/athopnfc/fragments/HomepageFragment.kt
@@ -1,18 +1,35 @@
 package com.example.athopnfc.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.example.athopnfc.LoginScreen
 import com.example.athopnfc.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-class HomepageFragment : Fragment() {
+class HomepageFragment : Fragment(){
+
+    private lateinit var btnLogOut: Button
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_homepage, container, false)
+        val view = inflater.inflate(R.layout.fragment_homepage, container, false)
+        auth = Firebase.auth
+        btnLogOut = view.findViewById(R.id.logOutButton)
+        btnLogOut.setOnClickListener {
+            val logoutIntent = Intent(view.context, LoginScreen::class.java)
+            logoutIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(logoutIntent)
+        }
+        return view
     }
 }
