fd557102fe58b7e3f52251a2a4ae31aca770a7d5
Raj Rathod <rajrasikrathod@gmail.com>
2022-05-05 12:17:14 +1200

    All add card info screens have been added
    This includes gym card, hop card, and loyalty card

--- app/src/main/java/com/example/athopnfc/AddHopCardScreen.kt
+++ app/src/main/java/com/example/athopnfc/AddHopCardScreen.kt
@@ -3,7 +3,7 @@ package com.example.athopnfc
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class AddHopCard : AppCompatActivity() {
+class AddHopCardScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/athopnfc/SelectCardScreen.kt
+++ app/src/main/java/com/example/athopnfc/SelectCardScreen.kt
@@ -7,19 +7,32 @@ import androidx.appcompat.app.AppCompatActivity
 
 class SelectCardScreen :AppCompatActivity() {
 
-    private lateinit var addHopCardBtn: Button;
+    private lateinit var addHopCardBtn: Button
+    private lateinit var addGymCardBtn: Button
+    private lateinit var addLoyaltyCardBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.add_cards)
 
-        addHopCardBtn = findViewById(R.id.hopCard);
+        addHopCardBtn = findViewById(R.id.hopCardBtn)
+        addGymCardBtn = findViewById(R.id.gymCardBtn)
+        addLoyaltyCardBtn = findViewById(R.id.loyaltyCardBtn)
 
         addHopCardBtn.setOnClickListener {
-            val intent = Intent(this, AddHopCard::class.java)
+            val intent = Intent(this, AddHopCardScreen::class.java)
+            startActivity(intent)
+        }
+
+        addGymCardBtn.setOnClickListener {
+            val intent = Intent(this, AddGymCardScreen::class.java)
+            startActivity(intent)
+        }
+
+        addLoyaltyCardBtn.setOnClickListener {
+            val intent = Intent(this, AddLoyaltyCardScreen::class.java)
             startActivity(intent)
         }
     }
-
 }
