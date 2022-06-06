0d30c1053883a909b3668a00e98dcd0194f8395e
Evilcarry <benjhithaxx@gmail.com>
2022-06-06 15:44:28 +1200

    The card now adds info to the homescreen

--- app/src/main/java/com/example/athopnfc/AddHopCardScreen.kt
+++ app/src/main/java/com/example/athopnfc/AddHopCardScreen.kt
@@ -1,13 +1,70 @@
 package com.example.athopnfc
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 //This class is so that the user can add a hop card
 class AddHopCardScreen : AppCompatActivity() {
 
+    private lateinit var cardNameField : EditText
+    private lateinit var cardNumberField : EditText
+    private lateinit var cardDescription : EditText
+    private lateinit var cardSaveBtn : Button
+
+    private companion object {
+        lateinit var auth: FirebaseAuth
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_athop_card)
+
+        cardNameField = findViewById(R.id.hopCardNameTxtInpt)
+        cardNumberField = findViewById(R.id.hopCardNoTxtInpt)
+        cardDescription = findViewById(R.id.hopCardDescTxtInpt)
+        cardSaveBtn = findViewById(R.id.saveAtHopCard)
+        val db = Firebase.firestore
+
+        cardSaveBtn.setOnClickListener{
+            if(validateEmptyString(cardNameField, cardNumberField, cardDescription)){
+                auth = FirebaseAuth.getInstance()
+
+                val docCard = hashMapOf(
+                    "cardName" to cardNameField.text.toString(),
+                    "cardNumber" to cardNumberField.text.toString(),
+                    "cardDesc" to cardDescription.text.toString()
+                )
+                val docWallet = hashMapOf(
+                    "HopCard" to docCard,
+                    "displayName" to auth.currentUser?.displayName.toString(),
+                    "email" to auth.currentUser?.email.toString()
+                )
+
+                db.collection("users").document(auth.currentUser?.email.toString())
+                    .set(docWallet)
+                    .addOnSuccessListener { Log.d(TAG, "Success")
+                        Toast.makeText(this@AddHopCardScreen, "Hop card successfully added.", Toast.LENGTH_SHORT)
+                            .show()
+                        startActivity(Intent(this, MainScreen::class.java))
+                    }
+                    .addOnFailureListener { e -> Log.w(TAG, "Error at uploading data", e)}
+            }
+        }
+    }
+
+
+    private fun validateEmptyString(cardName : EditText, cardNumber : EditText, cardDesc : EditText) : Boolean{
+        return cardName.text.toString().isNotBlank() && cardNumber.text.toString()
+            .isNotBlank() && cardDesc.text.toString().isNotBlank()
     }
 }
--- app/src/main/java/com/example/athopnfc/AddLoyaltyCardScreen.kt
+++ app/src/main/java/com/example/athopnfc/AddLoyaltyCardScreen.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 //This class is so that the user can add a loyalty card
 class AddLoyaltyCardScreen: AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_loyalty_card)
